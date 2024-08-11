ab29c5268592c34dc92f31bc27cf9810f4475f9d
Rafael Cruz <rcas2009@outlook.com>
2024-08-10 21:31:45 -0300

    Adding decimal points

--- dio-calculadora-react/src/pages/calculator.tsx
+++ dio-calculadora-react/src/pages/calculator.tsx
@@ -11,10 +11,23 @@ export function Calculator() {
     const lastChar = currentNumber.slice(-1);
 
     if (isResult && !isNaN(Number(number))) {
+      // If the result is displayed and the user clicks a number, start a new calculation
       setCurrentNumber(number);
+      setIsResult(false);
+    } else if (number === "." && !currentNumber.includes(".")) {
+      // If the number is a decimal point and there is no decimal point in the current number
+      if (operators.includes(lastChar) || currentNumber === "0") {
+        // Handle decimal point after an operator or leading zero
+        setCurrentNumber((prev) => prev + "0.");
+      } else {
+        // Add decimal point to the current number
+        setCurrentNumber((prev) => prev + ".");
+      }
     } else if (operators.includes(lastChar) && operators.includes(number)) {
+      // Prevent multiple operators in a row
       return;
     } else {
+      // Add number or operator to the current number
       setCurrentNumber((prev) =>
         prev === "0" && !operators.includes(number) ? number : prev + number
       );
@@ -28,10 +41,13 @@ export function Calculator() {
   };
 
   const handleCalculate = () => {
-    let expression = currentNumber.replace(/(\d*)√(\d+)/g, (_, p1, p2) => {
-      const prefix = p1 ? `${p1}*` : "";
-      return `${prefix}Math.sqrt(${p2})`;
-    });
+    let expression = currentNumber.replace(
+      /(\d*\.?\d*)√(\d*\.?\d+)/g,
+      (match, p1, p2) => {
+        const prefix = p1 ? `${p1}*` : ""; // Add multiplication if there's a prefix number
+        return `${prefix}Math.sqrt(${p2})`;
+      }
+    );
     const result = eval(expression.replace("^", "**"));
     setCurrentNumber(
       result === Infinity ? "Can't divide a number by 0" : result.toString()
@@ -63,8 +79,9 @@ export function Calculator() {
           <Button
             onClick={handleAddNumberText}
             label="0"
-            className="col-span-3"
+            className="col-span-2"
           />
+          <Button onClick={handleAddNumberText} label="." />
           <Button onClick={handleCalculate} label="=" />
         </div>
       </div>
