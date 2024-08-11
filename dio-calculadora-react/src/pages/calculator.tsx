f4c425c55646e6d4ac8a5c059e88471c486d1ff4
Rafael Cruz <rcas2009@outlook.com>
2024-08-10 21:19:46 -0300

    Add sqrt button + handling

--- dio-calculadora-react/src/pages/calculator.tsx
+++ dio-calculadora-react/src/pages/calculator.tsx
@@ -7,10 +7,17 @@ export function Calculator() {
   const [isResult, setIsResult] = useState(false);
 
   const handleAddNumberText = (number: string) => {
+    const operators = ["+", "-", "*", "^", "/"];
+    const lastChar = currentNumber.slice(-1);
+
     if (isResult && !isNaN(Number(number))) {
       setCurrentNumber(number);
+    } else if (operators.includes(lastChar) && operators.includes(number)) {
+      return;
     } else {
-      setCurrentNumber((prev) => (prev === "0" ? number : prev + number));
+      setCurrentNumber((prev) =>
+        prev === "0" && !operators.includes(number) ? number : prev + number
+      );
     }
     setIsResult(false);
   };
@@ -21,7 +28,11 @@ export function Calculator() {
   };
 
   const handleCalculate = () => {
-    const result = eval(currentNumber.replace("^", "**"));
+    let expression = currentNumber.replace(/(\d*)√(\d+)/g, (_, p1, p2) => {
+      const prefix = p1 ? `${p1}*` : "";
+      return `${prefix}Math.sqrt(${p2})`;
+    });
+    const result = eval(expression.replace("^", "**"));
     setCurrentNumber(
       result === Infinity ? "Can't divide a number by 0" : result.toString()
     );
@@ -33,27 +44,28 @@ export function Calculator() {
       <div className="bg-slate-400 items-center flex flex-col gap-3 py-4 px-2">
         <Input currentNumber={currentNumber}></Input>
         <div className="grid grid-rows-4 grid-cols-4 gap-3">
-          <Button onClick={handleAddNumberText} label="*" />
-          <Button onClick={handleAddNumberText} label="/" />
           <Button onClick={handleOnClear} label="C" />
-          <Button onClick={handleAddNumberText} label="^" />
+          <Button onClick={handleAddNumberText} label="/" />
+          <Button onClick={handleAddNumberText} label="*" />
+          <Button onClick={handleAddNumberText} label="-" />
           <Button onClick={handleAddNumberText} label="7" />
           <Button onClick={handleAddNumberText} label="8" />
           <Button onClick={handleAddNumberText} label="9" />
-          <Button onClick={handleAddNumberText} label="-" />
+          <Button onClick={handleAddNumberText} label="+" />
           <Button onClick={handleAddNumberText} label="4" />
           <Button onClick={handleAddNumberText} label="5" />
           <Button onClick={handleAddNumberText} label="6" />
-          <Button onClick={handleAddNumberText} label="+" />
+          <Button onClick={handleAddNumberText} label="^" />
           <Button onClick={handleAddNumberText} label="1" />
           <Button onClick={handleAddNumberText} label="2" />
           <Button onClick={handleAddNumberText} label="3" />
-          <Button onClick={handleCalculate} label="=" />
+          <Button onClick={handleAddNumberText} label="√" />
           <Button
             onClick={handleAddNumberText}
             label="0"
-            className="col-span-4"
+            className="col-span-3"
           />
+          <Button onClick={handleCalculate} label="=" />
         </div>
       </div>
     </div>
